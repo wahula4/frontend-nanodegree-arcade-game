cc453f822f2ab35633f09a0314090ebbfb875d5c
Tony Wahula <mawahula4@gmail.com>
2018-09-11 20:42:13 -0600

    added gulp for practice

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', function() {
+  console.log("hello gulp!")
+  gulp.watch('sass/**/*.scss', ['styles'])
+});
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));
+});
